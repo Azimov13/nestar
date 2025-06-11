bf5d8f7bcc4a276a3ace6cdc7ae7fe210009dcdd
Azimov13 <elyorbek_4999@mail.ru>
2025-06-11 19:14:41 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import {
-	InternalServerErrorException,
 	UsePipes,
 	ValidationPipe,
 } from '@nestjs/common';
