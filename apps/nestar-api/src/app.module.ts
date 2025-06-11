1e0a1389e719b2c996a3f487831b13dae4d69359
Azimov13 <elyorbek_4999@mail.ru>
2025-06-11 17:36:59 +0900

    feat: develop global GraphQl error handling system

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,6 +7,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { T } from './libs/types/common';
 
 @Module({
 	imports: [
@@ -16,6 +17,15 @@ import { DatabaseModule } from './database/database.module';
 			playground: true,
 			uploads: false,
 			autoSchemaFile: true,
+			formatError: (error: T) => {
+				console.log('error:', error);
+				const garphQLFormattedError = {
+					code: error?.extensions.code,
+					message: error?.extensions?.exception?.message || error?.extensions?.reponse?.message || error?.message,
+				};
+				console.log('GRAPQL GLOBAL ERR:', garphQLFormattedError);
+				return garphQLFormattedError;
+			},
 		}),
 		ComponentsModule, //HTTP
 		DatabaseModule, //TCP
