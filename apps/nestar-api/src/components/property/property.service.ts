44bf4fb6b1daa34b254b34f7348df46d2a0c415c
Azimov13 <elyorbek_4999@mail.ru>
2025-07-20 16:27:27 +0900

    feat: modify getProperties GraphQL API logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -196,10 +196,11 @@ export class PropertyService {
 			text,
 		} = input.search;
 		if (memberId) match.memberId = shapeIntoMongoObjectId(memberId);
-		if (locationList) match.propertyLocation = { $in: locationList };
-		if (roomList) match.propertyRooms = { $in: roomList };
-		if (bedsList) match.propertyBeds = { $in: bedsList };
-		if (typeList) match.propertyType = { $in: typeList };
+		if (locationList && locationList.length)
+			match.propertyLocation = { $in: locationList };
+		if (roomList && roomList.length) match.propertyRooms = { $in: roomList };
+		if (bedsList && bedsList.length) match.propertyBeds = { $in: bedsList };
+		if (typeList && typeList.length) match.propertyType = { $in: typeList };
 
 		if (pricesRange)
 			match.propertyPrice = { $gte: pricesRange.start, $lte: pricesRange.end };
@@ -232,9 +233,6 @@ export class PropertyService {
 	): Promise<Properties> {
 		return await this.likeService.getFavoriteProperties(memberId, input);
 	}
-
-
-
 
 	public async getAgentProperties(
 		memberId: ObjectId,
